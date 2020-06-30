3939bdf612fe30be1ff3f9cc8361cd5e8cb27a78
Enmanuel Toribio <enmanuel@torib.io>
2020-06-30 18:04:00 -0400

    Added AppcenterSDK

--- ThirtySixQuestions/App.xaml.cs
+++ ThirtySixQuestions/App.xaml.cs
@@ -8,6 +8,9 @@ using ThirtySixQuestions.Services;
 using ThirtySixQuestions.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace ThirtySixQuestions
 {
@@ -21,6 +24,11 @@ namespace ThirtySixQuestions
         protected override async void OnInitialized()
         {
             InitializeComponent();
+
+            AppCenter.Start("android=2cbf647c-a4d6-413d-8e05-7db31d72daa6;" +
+                     "uwp={Your UWP App secret here};" +
+                     "ios={Your iOS App secret here}",
+                     typeof(Analytics), typeof(Crashes));
 
             var result = await NavigationService.NavigateAsync("MainPage");
 
